fd10739de1d890586504a09c52b6e373d707a4d2
Douglas Luman <dluman@douglasjluman.com>
2020-01-31 10:11:21 -0500

    Initial commit

---
+++ src/main/java/practicaltwo/MathTrick.java
@@ -0,0 +1,29 @@
+package practicaltwo;
+
+/** Implements the "friendship" trick to practice operators.
+ *
+ * @author {Your Name Here}
+ */
+public class MathTrick {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    /*
+	 * TODO: Set up identifiers:
+     *		 cardA
+	 * 		 cardB
+	 *       workingTotal
+	 *       magicNumnber (see README)
+	 */
+    
+	/*
+	 * TODO: Perform the trick using the operators:
+	 *       +=, *=, -= and use the magicNumber
+	 */
+    
+	// TODO: Print a message to match the example in the README
+  }
+}
